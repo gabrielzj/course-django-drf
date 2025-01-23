22819d2a26a0ae1e05e4fb685e9bcb2ee428ee87
gabrielzj <gabrielzuinj@gmail.com>
2025-01-23 00:44:42 -0300

    update urls and views

--- recipes/views.py
+++ recipes/views.py
@@ -3,11 +3,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+# def home(request):
+#     return render(request, 'recipes/home.html')
+
+# example of passing a context to the template
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/pages/home.html', context={'nome': 'gabriel'})
 
-def contato(request):
-    return HttpResponse('contato')
-
-def sobre(request):
-    return HttpResponse('sobre')
