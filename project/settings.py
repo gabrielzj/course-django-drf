242e60e9389a1576d5b3ef5cee605f7825b0bc3f
gabrielzj <gabrielzuinj@gmail.com>
2025-04-18 02:36:18 -0300

    add: static config

--- project/settings.py
+++ project/settings.py
@@ -120,7 +120,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    # Add the path to the static files directory
+    BASE_DIR / 'static',
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
